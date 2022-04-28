d207b2bd5c31ba1dff9bb167a3868d972eef4dda
= <=>
2022-04-28 10:25:29 +0200

    Detalle finalizado

--- project_front/src/app/tienda/containers/MyProducto/my-producto/my-producto.component.ts
+++ project_front/src/app/tienda/containers/MyProducto/my-producto/my-producto.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-my-producto',
   templateUrl: './my-producto.component.html',
-  styleUrls: ['./my-producto.component.css']
+  styleUrls: ['./my-producto.component.css'],
+  providers: [MessageService]
 })
 export class MyProductoComponent implements OnInit {
   value19: number = 0;
   val3:number
-  constructor() {
+  constructor(private primengConfig: PrimeNGConfig,private messageService: MessageService) {
     this.val3 =3;
    }
 
   ngOnInit(): void {
   }
-
+  showInfo() {
+    this.messageService.add({severity:'info', summary: 'Info', detail: 'Añadido al carrito'});
+  }
 }
